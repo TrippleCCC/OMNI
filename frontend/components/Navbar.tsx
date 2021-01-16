73f79de5b915dc31792bb203cde0f93a2af459d7
TrippleCCC <chukwurahnonso@gmail.com>
2021-01-16 09:48:23 -0600

    first commit

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>OMNI</h1>
+    </div>
+  );
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <p1>Hello</p1>
+    </Layout>
+  )
+}
